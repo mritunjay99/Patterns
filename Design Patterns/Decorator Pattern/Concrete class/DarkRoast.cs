0804e6ee3e43d7050cec5a13c3ccb04efef562b5
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2024-09-12 23:56:58 +0530

    Decorator design pattern

---
+++ Design Patterns/Decorator Pattern/Concrete class/DarkRoast.cs
@@ -0,0 +1,20 @@
+﻿using Design_Patterns.Decorator_Pattern.Base_class;
+
+namespace Design_Patterns.Decorator_Pattern.Concrete_class
+{
+    public class DarkRoast:Beverage
+    {
+        //public DarkRoast()
+        //{
+        //    description = "Beverage coffee";
+        //}
+        public override string getDescription()
+        {
+            return "Beverage coffee";
+        }
+        public override double cost()
+        {
+            return 0.50;
+        }
+    }
+}
---
+++ Design Patterns/Decorator Pattern/Concrete class/Whip.cs
@@ -0,0 +1,24 @@
+﻿using Design_Patterns.Decorator_Pattern.Base_class;
+
+namespace Design_Patterns.Decorator_Pattern.Concrete_class
+{
+    public class Whip : CondimentDecorator
+    {
+        Beverage beverage;
+
+        public Whip(Beverage bvg)
+        {
+            beverage = bvg;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Whip";
+        }
+
+        public override double cost()
+        {
+            return beverage.cost() + 0.40;
+        }
+    }
+}
