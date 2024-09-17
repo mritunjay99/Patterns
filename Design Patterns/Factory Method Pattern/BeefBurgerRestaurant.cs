66f526ee8c31967ba4f247a0a61fa972250e0d0c
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2024-09-18 00:03:16 +0530

    Factory method design pattern

---
+++ Design Patterns/Factory Method Pattern/BeefBurgerRestaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Factory_Method_Pattern
+{
+    public class BeefBurgerRestaurant : Restaurant
+    {
+        public override Burger createBurger()
+        {
+            return new BeefBurger();
+        }
+    }
+}
