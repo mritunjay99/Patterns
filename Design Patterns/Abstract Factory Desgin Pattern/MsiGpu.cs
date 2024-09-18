965dee2f5bfdc87e4ab5a019ccc6683baa8afa99
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-09-18 23:34:21 +0530

    Abstract factory design pattern

---
+++ Design Patterns/Abstract Factory Desgin Pattern/MsiGpu.cs
@@ -0,0 +1,10 @@
+﻿namespace Design_Patterns.Abstract_Factory_Desgin_Pattern
+{
+    public class MsiGpu : Gpu
+    {
+        public void assemble()
+        {
+            Console.WriteLine("MsiGpu assembled!");
+        }
+    }
+}
