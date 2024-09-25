53a111369b6efcebbddeb9e9a21af9aacfbc3b2c
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-09-25 23:22:00 +0530

    Command pattern
    * Added command pattern
    
    * Added undo functionality

---
+++ Design Patterns/Command Pattern/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Command_Pattern.Command
+{
+    public interface ICommand
+    {
+        void Execute();
+        void undo();
+    }
+}
---
+++ Design Patterns/Command Pattern/Command/TurnOffACCommand.cs
@@ -0,0 +1,29 @@
+﻿using Design_Patterns.Command_Pattern.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Command_Pattern.Command
+{
+    public class TurnOffACCommand : ICommand
+    {
+        AcController Controller;
+
+        public TurnOffACCommand(AcController ac)
+        {
+            Controller = ac;
+        }
+
+        public void Execute()
+        {
+            Controller.turnOffAC();
+        }
+
+        public void undo()
+        {
+            Controller.turnOnAC();
+        }
+    }
+}
