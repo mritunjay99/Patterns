59b4bb089a04e236ebb5e608c4f3fd7d738b97c0
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-09-30 22:49:39 +0530

    Added template method pattern

---
+++ Design Patterns/Template Method Pattern/Tea.cs
@@ -0,0 +1,15 @@
+﻿namespace Design_Patterns.Template_Method_Pattern
+{
+    public class Tea : CaffeineBeverage
+    {
+        public override void addCondiments()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+
+        public override void brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+    }
+}
