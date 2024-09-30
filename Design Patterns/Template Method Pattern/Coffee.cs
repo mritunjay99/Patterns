e2d479451ee47d69df150007d840fe68d902f10f
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-09-30 22:49:39 +0530

    Added template method pattern

---
+++ Design Patterns/Template Method Pattern/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Design_Patterns.Template_Method_Pattern
+{
+    public class Coffee : CaffeineBeverage
+    {
+        public override void addCondiments()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+
+        public override void brew()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+    }
+}
