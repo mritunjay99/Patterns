775b316e63ee454b2edd386450be6d3f14ef0127
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2024-10-02 21:37:17 +0530

    Added Facade pattern

---
+++ Design Patterns/Facade Pattern/Waiter_Subsystem1.cs
@@ -0,0 +1,20 @@
+﻿namespace Design_Patterns.Facade_Pattern
+{
+    public class Waiter_Subsystem1
+    {
+        public void writeOrder()
+        {
+            Console.WriteLine("Waiter writes client's order.");
+        }
+
+        public void sendToKitchen()
+        {
+            Console.WriteLine("Send order to kitchen.");
+        }
+
+        public void serveCustomer()
+        {
+            Console.WriteLine("Yeei customer is served!!!");
+        }
+    }
+}
