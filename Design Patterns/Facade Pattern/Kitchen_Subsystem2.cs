d5afd0f51cd3897382222616bf0e5c4048aa7a05
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-10-02 21:38:36 +0530

    Added Facade pattern

---
+++ Design Patterns/Facade Pattern/Kitchen_Subsystem2.cs
@@ -0,0 +1,20 @@
+﻿namespace Design_Patterns.Facade_Pattern
+{
+    public class Kitchen_Subsystem2
+    {
+        public void prepareFood()
+        {
+            Console.WriteLine("Cook food.");
+        }
+
+        public void callWaiter()
+        {
+            Console.WriteLine("Call waiter.");
+        }
+
+        public void washDishes()
+        {
+            Console.WriteLine("Wash the dishes.");
+        }
+    }
+}
