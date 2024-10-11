66b1b8f9506e87c50c2aea95d7c4fef8e93dab2b
Mritunjay Yadav <168256518+mritunjayYadav88@users.noreply.github.com>
2024-10-11 12:17:06 +0530

    Added iterator pattern

---
+++ Design Patterns/Iterator Design Pattern/ListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace Design_Patterns.Iterator_Design_Pattern
+{
+    public class ListNode
+    {
+        public Channel ch;
+        public ListNode next;
+
+        public ListNode(Channel ch)
+        {
+            this.ch = ch;
+            next = null;
+        }
+    }
+}
